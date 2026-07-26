1fe192c5328f4a6476c04b0c7d33fc115c7b5ad6
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-26 21:28:29 +0600

    use PostRouter in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application } from "express";
+import { postRouter } from "./modules/post/post.router";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello, Wolrd!");
