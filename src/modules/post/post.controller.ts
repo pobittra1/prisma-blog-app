2acd543e057e4be3df5ab8210cffd5491efacd08
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-26 22:47:15 +0600

    post service logic

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
+import { postService } from "./post.service";
+import { error } from "node:console";
 
 const createPost = async (req: Request, res: Response) => {
-    console.log(req, res);
+    try {
+        const result = await postService.createPost(req.body)
+        res.status(201).json(result)
+    } catch (e) {
+        res.status(400).json({
+            error: "Post creation failed",
+            details: e
+        })
+    }
 }
 
 export const PostController = {
