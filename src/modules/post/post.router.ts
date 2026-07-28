7178289ec7eef14a8ea2851edde73fc74e8173b7
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-28 09:28:38 +0600

    set middleware: get session

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,8 +1,20 @@
-import express, { Router } from 'express';
+import express, { NextFunction, Request, Response, Router } from 'express';
 import { PostController } from './post.controller';
+import { auth as betterAuth } from "../../lib/auth";
 
 const router = express.Router();
 
-router.post("/", PostController.createPost)
+const auth = (...roles: any) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        // get user session
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
+        })
+        console.log(session);
+
+    }
+}
+
+router.post("/", auth("USER"), PostController.createPost)
 
 export const postRouter: Router = router;
