5dcbbda65f8f41a8a4dc72bfbcd9e1dd645a2070
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-07-29 11:33:09 +0600

    explore search query

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -22,7 +22,11 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPost = async (req: Request, res: Response) => {
     try {
-        const result = await postService.getAllPost();
+
+        // get query data of search name query
+        const { search } = req.query;
+        const searchString = typeof search === 'string' ? search : undefined;
+        const result = await postService.getAllPost({ search: searchString });
         res.status(200).json(result);
 
     } catch (err) {
