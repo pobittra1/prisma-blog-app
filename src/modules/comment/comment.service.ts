7fa87fa88a61ec3f86dd180b527cecc4f6c0fd61
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-01 16:16:26 +0600

    comment creation done

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,10 +1,28 @@
+import { prisma } from "../../lib/prisma"
+
 const createComment = async (payload: {
     content: string,
     authorId: string,
     postId: string,
     parentId?: string
 }) => {
-    console.log("create comment service", payload);
+    const postData = await prisma.post.findUniqueOrThrow({
+        where: {
+            id: payload.postId
+        }
+    })
+
+    if (payload.parentId) {
+        const parentData = await prisma.comment.findUniqueOrThrow({
+            where: {
+                id: payload.parentId
+            }
+        })
+    }
+
+    return await prisma.comment.create({
+        data: payload
+    })
 }
 
 export const ComemntService = {
