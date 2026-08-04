7defd2da7486884c06f85f6ada89fc710439c376
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2026-08-04 08:38:32 +0600

    handling prismaClientValidationError

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
 
 function errorHandler(
     err: any,
@@ -6,10 +7,19 @@ function errorHandler(
     res: Response,
     next: NextFunction) {
 
-    res.status(500);
+
+    let statusCode = 500;
+    let errorMessage = "Internal server error!";
+    let errorDetails = err;
+
+    if (err instanceof Prisma.PrismaClientValidationError) {
+        statusCode = 400;
+        errorMessage = "You provided incorrect field type or missing fields!";
+    }
+    res.status(statusCode);
     res.json({
-        message: "Error from error handler",
-        error: err
+        message: errorMessage,
+        error: errorDetails
     })
 }
 
